0ad552704202bbb6c5f35e96715ca3d8d18f894d
Akashk726 <kistapuramakash@gmail.com>
2025-10-08 07:09:50 +0000

    Fix Docker build

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h1>Welcome to My Next.js App</h1>
-      <p>"This is a simple application"</p>
+      <p>This is a simple application</p>
     </div>
   );
 };
