2f4c88b9074214d254ef71570575d048cdbb6c4d
Akashk726 <kistapuramakash@gmail.com>
2025-10-08 04:45:23 +0000

    Initial Next.js app setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Welcome to My Next.js App</h1>
+      <p>"This is a simple application"</p>
+    </div>
+  );
+};
+
+export default Home;
